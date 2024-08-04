b1b9e5b379f98bb58e73db49b7dec5d0cbf7e0e1
Ahsan ullah <ahsanullah0216@gmail.com>
2024-08-04 13:33:50 +0600

    create post factory and seeder to insert fake data in db

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'description'=>fake()->word(),
+            'author'=>fake()->name(),
         ];
     }
 }
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('posts')->truncate();
+        $posts = \App\Models\Post::factory(15)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    }
+}
